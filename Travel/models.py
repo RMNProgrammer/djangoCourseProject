15dea8ecda9c10d340d8da37339ee04d205e4c3c
RMNProgrammer <mohammadaminrmn.programmer@gmail.com>
2023-02-12 17:39:10 +0330

    Creating the first part of the database tables

--- Travel/models.py
+++ Travel/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=90)
+    email = models.EmailField()
+    subject = models.CharField(max_length=40)
+    message = models.TextField()
+    created_data = models.DateTimeField(auto_now_add=True)
+    update_data = models.DateTimeField(auto_now=True)

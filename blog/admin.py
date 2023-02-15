bae776d776ff19f9ad9955e57cfddee5d50f5a20
RMNProgrammer <mohammadaminrmn.programmer@gmail.com>
2023-02-15 19:39:28 +0330

    Display unique images for each post
    + Adding the image field to the blog database
    + Adding a photo upload section to the admin panel ( choose and change )
    + Extracting database information and displaying it on the blog page
    + Adding some new photos to the project
    + Updating the required libraries file of the project

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title','author','created_date','published_date','counted_views')
-    fields = ('author','title','content','published_date')
+    fields = ('author','title','content','image','published_date')
     #added to models.py
     #ordering = ['title']
     search_fields = ['title','content']

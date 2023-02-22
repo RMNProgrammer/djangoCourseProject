ecac95fda17ef75127a30e1af9e85929b2b704ca
RMNProgrammer <mohammadaminrmn.programmer@gmail.com>
2023-02-22 18:19:25 +0330

    Practicing database and admin topics - part 1 & 2

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,16 @@ def blog(request):
 def posts(request,PostID):
     published_posts = Post.objects.filter(published_date__lte=datetime.datetime.now())
     currentPost = get_object_or_404(published_posts,id=PostID)
+    if published_posts.first().id != currentPost.id:
+        next_posts = published_posts.filter(published_date__gt=currentPost.published_date)
+        next_post = next_posts[next_posts.count()-1]
+    else:
+        next_post = None
+    if published_posts.last().id != currentPost.id:
+        pre_post = published_posts.filter(published_date__lt=currentPost.published_date)[0]
+    else:
+        pre_post = None
     currentPost.counted_views += 1
     currentPost.save()
-    context = {'post':currentPost}
+    context = {'post':currentPost,'next_post':next_post,'pre_post':pre_post}
     return render(request,'blog/posts.html',context)

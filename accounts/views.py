467786ff4e3dd2e86184aa584a82999816fa07c7
RMNProgrammer <mohammadaminrmn.programmer@gmail.com>
2023-04-01 15:29:16 +0430

    User Authentication - Part 2
    Detection of user login in:
    * views.py
    * template file

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,11 @@ def sign_up(request):
     return render(request,'accounts/sign_up.html')
 
 def login(request):
-    return render(request,'accounts/login.html')
+    if request.user.is_authenticated:
+        msg = f'user is authenticated as {request.user.username}'
+    else:
+        msg = 'user is not authenticated'
+    return render(request,'accounts/login.html',{'message':msg})
 
 #def logout(request):
 #    return

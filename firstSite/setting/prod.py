d5588191771c2da27fbbfeb2b69c54ed1ecb7810
RMNProgrammer <mohammadaminrmn.programmer@gmail.com>
2023-05-11 17:31:44 +0430

    Important points of running the site on the host

--- firstSite/setting/prod.py
+++ firstSite/setting/prod.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-$fm0(&_x3$8w8w$u4qp!=73$=tvin5qfceq#nrqcnem3f!yjs=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1:8000"]
 
 #site framework
 SITE_ID = 2
@@ -29,4 +29,26 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [ BASE_DIR / "statics"]
 
-#CSRF_COOKIE_SECURE = True
+#---site security section---
+
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+CSRF_COOKIE_SECURE = True
+
+#X-Content-Type-Options
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Strict-Transport-Security
+SECURE_HSTS_SECONDS = 15768000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# that requests over HTTP are redirected to HTTPS. aslo can config in webserver
+SECURE_SSL_REDIRECT = True 
+
+# for more security
+CSRF_COOKIE_SECURE = True
+CSRF_USE_SESSIONS = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = 'Strict'
